c35b6f29dcf4d76ff232b7c57d4fd51e97ee3acd
Ostaptsov Danil <dost4657@gmail.com>
2022-09-29 15:42:12 +0300

    reques to create a new student and to output students collection

--- the-list-be/server.js
+++ the-list-be/server.js
@@ -1,8 +1,25 @@
 import Koa from 'koa';
 import Router from '@koa/router';
+import { MongoClient } from 'mongodb';
 
 const app = new Koa();
 const router = new Router();
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+const db = client.db('database');
+
+const students = db.collection('students');
+
+router.post('/student', async ctx => {
+  const insertResult = students.insertOne(ctx.request.body);
+  ctx.body = ('Insered documents => ', insertResult);
+})
+
+router.get('/students', async ctx => {
+  const findResult = await students.find({}).toArray();
+  ctx.body = findResult;
+})
 
 router.get('/', async ctx => {
   ctx.body = 'Hello World';
@@ -10,6 +27,7 @@ router.get('/', async ctx => {
 
 router.get('/hello/:name', async ctx => {
   ctx.body = `Hello ${ctx.params.name}`;
+  console.log('Complete');
 });
 
 app
