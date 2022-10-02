8dc8b4da11abb2cd96af3c610460bb7578e4bd99
Ostaptsov Danil <64201643+OstaptsovDanil@users.noreply.github.com>
2022-10-02 22:52:39 +0300

    [BE] Add APIs for adding, listing and deleting students (#11)
    Closes #8, closes #9, and closes #13.
    
    два запроса
    POST /students
    
    В теле запроса должен находиться JSON файл, который добавится в
    коллекцию студентов
    
    GET /students
    Возвращает коллекцию студентов
    
    Co-authored-by: Yurij Mikhalevich <yurij@mikhalevi.ch>
    Co-authored-by: Artem <roha.r@inbox.ru>

--- the-list-be/server.js
+++ the-list-be/server.js
@@ -1,19 +1,35 @@
 import Koa from 'koa';
 import Router from '@koa/router';
+import { MongoClient, ObjectId } from 'mongodb';
+import koaBody from 'koa-body';
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', async ctx => {
-  ctx.body = 'Hello World';
-});
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+const db = client.db('database');
+const students = db.collection('students');
 
-router.get('/hello/:name', async ctx => {
-  ctx.body = `Hello ${ctx.params.name}`;
-});
+router.post('/students', async ctx => {
+  const insertResult = await students.insertOne(ctx.request.body);
+  ctx.body = insertResult;
+})
+
+router.get('/students', async ctx => {
+  const findResult = await students.find({}).toArray();
+  ctx.body = findResult;
+})
+
+router.delete('/students/:id', async ctx =>{
+    const studentDelete = await students.deleteOne({_id: new ObjectId(ctx.params.id)});
+    ctx.body = studentDelete;
+})
 
 app
+  .use(koaBody())
   .use(router.routes())
   .use(router.allowedMethods());
+
 
 app.listen(4000);
