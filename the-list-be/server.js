12f652ed05f963a57b05d43e1121871666ff6ce0
Artem <roha.r@inbox.ru>
2022-10-05 12:07:13 +0300

    Сделал вывод списка всех айдишников на сегодняшнюю дату

--- the-list-be/server.js
+++ the-list-be/server.js
@@ -13,39 +13,40 @@ const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
 const db = client.db('database');
 const students = db.collection('students');
-const dailyAttendance = db.collection('dailyAttendance');
+const date = db.collection('date');
 
-router.post('/students/dailyAttendance', async ctx => {
-    const studID = ctx.request.body._id;
-    const studentInDate = {
-        date: ctx.request.body.date,
+router.post('/students/date', async ctx => {
+    const studentlist = await students.find({}).toArray();
+    let studID = [];
+    for(let i =0;studentlist.length>i; i++){
+        studID.push(studentlist[i]._id);
+      }
+    const StudentInDate = {
+        date: getTodayDate(),
         students: [studID]
     }
-    await dailyAttendance.updateOne({date: "06.10.2022"}, {$addToSet: {students: studID}});
-    const check = await dailyAttendance.find({}).toArray();
-    ctx.body = check;
-})
-
-router.get('/students/dailyAttendance', async ctx => {
-    const FindStudentsInDate = await dailyAttendance.find({}).toArray();
+    const insertStudentInDate = await date.insertOne(StudentInDate);
+    ctx.body = insertStudentInDate;
+  })
+  router.get('/students/date', async ctx => {
+    const FindStudentsInDate = await date.find({}).toArray();
     ctx.body = FindStudentsInDate;
-})
+  })
 
 
 router.post('/student', async ctx => {
-    const insertStudent = await students.insertOne(ctx.request.body);
-    ctx.body = insertStudent;
+  const insertStudent = await students.insertOne(ctx.request.body);
+  ctx.body = insertStudent;
 })
 
 router.get('/students', async ctx => {
-    const findStudents = await students.find({}).toArray();
-    ctx.body = findStudents;
+  const findStudents= await students.find({}).toArray();
+
+  ctx.body = findStudents;
 })
 
-router.delete('/students/:id', async ctx => {
-    const deleteStudent = await students.deleteOne({
-        _id: new ObjectId(ctx.params.id)
-    });
+router.delete('/students/:id', async ctx =>{
+    const deleteStudent = await students.deleteOne({_id: new ObjectId(ctx.params.id)});
     ctx.body = deleteStudent;
 })
 
@@ -58,7 +59,7 @@ app.use(koaBody()).use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000);
 
-function getTodayDate() {
+function getTodayDate(){
     const todayDate = new Date();
-    return (todayDate.getDate() + "." + (todayDate.getMonth() + 1) + "." + todayDate.getFullYear());
+    return (todayDate.getDate()+"."+(todayDate.getMonth()+1)+"."+todayDate.getFullYear());
 }
