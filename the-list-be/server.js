ee70307bf0361295d700e1415fb0836d4518288e
Irina <63745301+KogayIrina@users.noreply.github.com>
2022-10-06 19:18:43 +0300

    Add prettier (#21)
    ## Что делает этот PR?
    
    Добавила prettier. Он будет форматировать ваш код и заодно
    отформатировала и бекенд и фронтенд.
    Вы можете посмотреть в каких файлах код не отформатирован запустив
    команду:
    `npm run lint`
    внутри the-list-ui или the-list-be
    и можете отформатировать весь ваш код, запустив команду:
    `npm run lint:fix`
    
    ## Ограничения
    Форматирование не осуществляется автоматически перед коммитом, линтер
    нужно запускать перед тем, как создать коммит самостоятельно.
    
    ## Как тестировать?
    
    запустить команду: `npm run lint`
    посмотреть на перечень файлов, где есть ошибки.
    запустить команду: `npm run lint:fix`
    снова запустить команду: `npm run lint` и убедиться, что ошибок в файлах
    больше нет.
    
    ## Чеклист
    
    - [x] мой PR сфокусирован и содержит одно логически цельное изменение
    - [x] я протестировал(а) PR локально и убедился(ась) в том, что он
    работает
    - [ ] если моё изменение содержит изменения интерфейса, я вставил(а) в
    описание скриншот или скринкаст

--- the-list-be/server.js
+++ the-list-be/server.js
@@ -14,22 +14,20 @@ const students = db.collection('students');
 router.post('/students', async ctx => {
   const insertResult = await students.insertOne(ctx.request.body);
   ctx.body = insertResult;
-})
+});
 
 router.get('/students', async ctx => {
   const findResult = await students.find({}).toArray();
   ctx.body = findResult;
-})
+});
 
-router.delete('/students/:id', async ctx =>{
-    const studentDelete = await students.deleteOne({_id: new ObjectId(ctx.params.id)});
-    ctx.body = studentDelete;
-})
+router.delete('/students/:id', async ctx => {
+  const studentDelete = await students.deleteOne({
+    _id: new ObjectId(ctx.params.id)
+  });
+  ctx.body = studentDelete;
+});
 
-app
-  .use(koaBody())
-  .use(router.routes())
-  .use(router.allowedMethods());
-
+app.use(koaBody()).use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000);
