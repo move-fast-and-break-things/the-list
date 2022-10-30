e2d402d3b2632ef4e0c793dec9479c36bfa17475
Ostaptsov Danil <dost4657@gmail.com>
2022-10-30 23:18:24 +0300

    Сделал вывод студентов на экран

--- the-list-ui/src/App.js
+++ the-list-ui/src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Input from './components/Input';
+import { getStudents } from './request-functions';
 
 function App() {
+  const [students, setStudents] = useState();
+  const [studentsLoadingError, setStudentsLoadingError] = useState();
+
+  var divStudents = [];
+  if(!studentsLoadingError){
+    for(let i = 0; i < students?.length; i++){
+      divStudents.push(<div key={students[i]._id}>{students[i].name}</div>)
+    }
+  }
+
+  console.log(students);
+
+  useEffect(() => {
+    getStudents()
+      .then(setStudents)
+      .catch(setStudentsLoadingError);
+  }, []);
   return (
     <div className="App">
+      {studentsLoadingError || divStudents}
       <Input />
     </div>
   );
