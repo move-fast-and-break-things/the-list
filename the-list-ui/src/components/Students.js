5a8030833ae189278f2e4696a23773e87ef47e2f
EmmaMaltseva <92587254+EmmaMaltseva@users.noreply.github.com>
2022-11-06 20:08:15 +0300

    Стилизация вывода студентов (#37)
    ## Что делает этот PR?
    
    Стилизация вывода студентов: номера рядом со студентами, подчеркивающая
    линия
    
    ## Ограничения
    Нет
    
    ![image](https://user-images.githubusercontent.com/92587254/200012247-59182441-88c8-49f9-a7b9-21102d1b5825.png)
    
    
    ## Как тестировать?
    
    Запустить the-list-be и the-list-ui командой npm start. На открывшемся
    сайте выведется список студентов. Если списка нет, то добавить в окне,
    нажать кнопку "добавить" и обновить страницу
    
    ## Чеклист
    
    - [x] мой PR сфокусирован и содержит одно логически цельное изменение
    - [x] я протестировал(а) PR локально и убедился(ась) в том, что он
    работает
    - [x] если моё изменение содержит изменения интерфейса, я вставил(а) в
    описание скриншот или скринкаст

--- the-list-ui/src/components/Students.js
+++ the-list-ui/src/components/Students.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getStudents } from '../request-functions';
+import './Students.css';
 
 export default function Students() {
   const [students, setStudents] = useState([]);
@@ -14,9 +15,13 @@ export default function Students() {
     return <p>{studentsLoadingError}</p>;
   } else {
     for (let i = 0; i < students.length; i++) {
-      divStudents.push(<div key={students[i]._id}>{students[i].name}</div>);
+      divStudents.push(
+        <div className="one-student" key={students[i]._id}>
+          <div className="number">{i + 1}</div>
+          <div className="name-one-student">{students[i].name}</div>
+        </div>
+      );
     }
-
     return <div className="Students">{divStudents}</div>;
   }
 }
