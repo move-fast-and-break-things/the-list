003bcbc31350fef460f869b47cecc70ba571ba4e
Emma Maltseva <emmamaltseva0@gmail.com>
2022-11-24 23:07:51 +0300

    добавлена галочка, инпут и галочка появляются при нажати добавления пользователя

--- the-list-ui/src/components/Input.js
+++ the-list-ui/src/components/Input.js
@@ -9,15 +9,22 @@ export default function Input() {
     postStudents(name);
     setName('');
   };
+  function activationInput() {
+    var el = document.getElementById("input-person");
+    var buttonPost = document.getElementById("button-post-user");
+    buttonPost.style.visibility = 'visible';
+    el.focus();
+  }
   return (
     <form className="form-input" onSubmit={onAdd}>
+      <button className="button-add-user" onClick={activationInput} type="button"></button>
       <input
         className="input-person"
-        placeholder="Введите имя студента"
+        id = "input-person"
         value={name}
         onChange={event => setName(event.target.value)}
       />
-      <button className="button-add-user" type="submit"></button>
+      <button className="button-post-user" type="submit" id="button-post-user"></button>
     </form>
   );
 }
