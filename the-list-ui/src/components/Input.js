449eb9510a38baa305641b2dce2bf2ce7f444fab
Emma Maltseva <emmamaltseva0@gmail.com>
2022-11-24 23:11:29 +0300

    launched the linter

--- the-list-ui/src/components/Input.js
+++ the-list-ui/src/components/Input.js
@@ -10,21 +10,29 @@ export default function Input() {
     setName('');
   };
   function activationInput() {
-    var el = document.getElementById("input-person");
-    var buttonPost = document.getElementById("button-post-user");
+    var el = document.getElementById('input-person');
+    var buttonPost = document.getElementById('button-post-user');
     buttonPost.style.visibility = 'visible';
     el.focus();
   }
   return (
     <form className="form-input" onSubmit={onAdd}>
-      <button className="button-add-user" onClick={activationInput} type="button"></button>
+      <button
+        className="button-add-user"
+        onClick={activationInput}
+        type="button"
+      ></button>
       <input
         className="input-person"
-        id = "input-person"
+        id="input-person"
         value={name}
         onChange={event => setName(event.target.value)}
       />
-      <button className="button-post-user" type="submit" id="button-post-user"></button>
+      <button
+        className="button-post-user"
+        type="submit"
+        id="button-post-user"
+      ></button>
     </form>
   );
 }
