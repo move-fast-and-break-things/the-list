0737e0fb5cff6973db95c60fe53237759c1c203f
Artem Belyi <roha.r@inbox.ru>
2023-01-29 19:04:28 +0300

    Кнопка удаления студента (#39)

--- the-list-ui/src/components/Students.js
+++ the-list-ui/src/components/Students.js
@@ -1,8 +1,12 @@
 import { useStudents } from '../apiHooks';
 import './Students.css';
+import deleteIcon from './images/delete.svg';
+import { useDeleteStudent } from '../apiHooks';
 
 export default function Students() {
   const { data: students, error: studentsLoadingError } = useStudents();
+  const { mutate: deleteStudent, isLoading: isDeletingStudent } =
+    useDeleteStudent();
 
   const divStudents = [];
 
@@ -14,6 +18,13 @@ export default function Students() {
         <div className="one-student" key={students[i]._id}>
           <div className="number">{i + 1}</div>
           <div className="name-one-student">{students[i].name}</div>
+          <button
+            disabled={isDeletingStudent}
+            className="delete-icon"
+            onClick={() => deleteStudent(students[i]._id)}
+          >
+            <img src={deleteIcon} alt="delete icon" />
+          </button>
         </div>
       );
     }
